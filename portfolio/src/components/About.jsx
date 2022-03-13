69ace8da46c39b54a2f4fffa56dba5bda0fb5af5
skbhoi2001 <soumyak.bhoi.2001@gmail.com>
2022-03-13 16:19:30 +0530

    S2

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -10,8 +10,11 @@ const About = () => {
                 <div class="col-md-7 order-md-2">
                     <h1 class="featurette-heading about-head">ABOUT</h1>
                     <p class="lead summary-text">A passionate aspiring Full Stack Developer skilled in MERN stack, able to apply programming skills to solve complex challenges and work on technologies like HTML, CSS, JavaScript, React, Redux, Material-UI, Tailwind CSS.Eager to gain a dynamic and challenging role in the field of Software Development that will offer the opportunity for further development of my abilities, skills, and knowledge in an organization.</p>
+                    <div className='links' >
                     <a href="https://github.com/skbhoi2001" rel="noreferrer" target="_blank" ><i class="fab fa-github fa-2x github-logo"></i></a>
                     <a href="https://www.linkedin.com/in/soumyak-kumar-bhoi-3b73a4184/" rel="noreferrer" target="_blank" ><i class="fab fa-linkedin-in fa-2x linkedin-logo"></i></a>
+                    <a className="resumes" href="https://drive.google.com/file/d/1Me0ppnBHdL1_awIlEwKoDD6NGMSu8xEx/view?usp=sharing" target="_blank" rel="noreferrer">Resume <span className="donld-icon"><i class="fas fa-download"></i></span></a>
+                    </div>
                 </div>
             </div>
         </div>
